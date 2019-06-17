c842a7a6c3bd1bfc0b21d1b1ce75d5e684a200a3
wgd <94259561@qq.com>
2019-06-17 23:35:46 +0800

    把本地工程添加到git

---
+++ src/com/itheima/git/Demo01.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello baby");
+        System.out.println("hello baby");
+        System.out.println("hello baby");
+        System.out.println("hello baby");
+        System.out.println("hello baby");
+    }
+}
